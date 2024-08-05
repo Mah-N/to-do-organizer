223c73075db22f2b5882dd1c22a50e38aa4c4145
Nisha Mahajan <nisha.mah25@gmail.com>
2024-08-05 20:24:12 +0000

    Add Task form

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,3 +23,4 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('', include('my_task.urls'), name='home'),
 ]
+
