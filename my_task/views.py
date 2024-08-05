9147ed10e1588fc4acc1c4afdb462f8448e1a6e8
Nisha Mahajan <nisha.mah25@gmail.com>
2024-08-05 20:36:28 +0000

    Add views for task listing and creation

--- my_task/views.py
+++ my_task/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Task
+from .forms import TaskForm
 
 class HomePage(TemplateView):
     """
@@ -8,3 +11,13 @@ class HomePage(TemplateView):
     template_name = 'home.html'
 
 # Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task_form.html'
+    success_url = reverse_lazy('task_list')
