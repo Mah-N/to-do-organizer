dddf8788d1988b357a95d0d7b7b89894cc5470ef
Nisha Mahajan <nisha.mah25@gmail.com>
2024-08-05 23:18:08 +0000

    Update Taskform to handle date-time

--- my_task/forms.py
+++ my_task/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateTimeField(
+        widget=forms.TextInput(attrs={
+            'type': 'datetime-local',
+            'class': 'form-control'
+        })
+    )
+
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'priority', 'status']
