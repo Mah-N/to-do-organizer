138d9c7080fc12a6ff1bc2dc12542f378f7c0543
Nisha Mahajan <nisha.mah25@gmail.com>
2024-08-08 12:13:22 +0000

    Add success message for task update

--- my_task/views.py
+++ my_task/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
+# from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from .models import Task
 from .forms import TaskForm
@@ -39,8 +40,17 @@ class TaskUpdateView(UpdateView):
     form_class = TaskForm
     template_name = 'task_form.html'
     success_url = reverse_lazy('task_list')
+    
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Task updated successfully!')
+        return super().form_valid(form)
 
 class TaskDeleteView(DeleteView):
     model = Task
     template_name = 'task_delete.html'
-    success_url = reverse_lazy('task_list')    
+    success_url = reverse_lazy('task_list')
+
+    # def delete(self, request, *args, **kwargs):
+     #   messages.success(self.request, 'Task deleted successfully!')
+      #  return super().delete(request, *args, **kwargs)    
