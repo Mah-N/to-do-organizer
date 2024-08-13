29fccb48e3dcc2a6064c3f6525d0ecabf4cc703a
Nisha Mahajan <nisha.mah25@gmail.com>
2024-08-13 17:03:45 +0000

    Update setting.py to resolve deployed link issue

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -32,14 +32,14 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.codeinstitute-ide.net/',
     'https://*.herokuapp.com',
     'https://8000-mahn-todoorganizer-fqmlgm3guvk.ws.codeinstitute-ide.net',
-    'https://my-todo-list-final-project-45103fba3130.herokuapp.com'
+    'https://my-todo-list-final-project-45103fba3130.herokuapp.com',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
 
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mahn-todoorganizer-fqmlgm3guvk.ws.codeinstitute-ide.net','my-todo-list-final-project-45103fba3130.herokuapp.com','.herokuapp.com']
 
